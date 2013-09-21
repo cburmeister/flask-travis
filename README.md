e3e5baa3e3e297d4befc63aef3eb888eeabc84df
Corey Burmeister <burmeister.corey@gmail.com>
2013-09-21 11:53:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+flask-travis
+===========
+
+Easily fetch TravisCI environment variables when testing.
+
+## Install
+
+```python
+pip install Flask-Travis
+```
+
+## Usage
+
+Import and initialize:
+
+```python
+from flask.ext.import Flask-Travis
+
+app = Flask(__name__)
+
+travis = Travis(app)
+```
+
+Or better yet, use Flask's application factory pattern:
+
+```python
+travis.init_app(app)
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='Flask-Travis',
+    version='0.0.1',
+    url='https://github.com/cburmeister/flask-travis',
+    license='MIT',
+    author='Corey Burmeister',
+    author_email='cburmeister@discogs.com',
+    description='Easily fetch Travis CI environment variables when testing.',
+    long_description=__doc__,
+    py_modules=['flask_travis'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
